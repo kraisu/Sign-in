ebb5c456dbd1316b902e3f9bc55a5f1c48e8db7e
kraisu <s1900885@edu.bc.fi>
2020-09-15 11:17:11 +0300

    ok

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const btnSignIn = document.querySelector("#btnSignIn");
 
 btnSignIn.onclick = function () {
   const username = document.querySelector("#username").value;
-  const password = document.querySelector("#password1");
+  const password1 = document.querySelector("#password1").value;
+
+  const tiedot = document.querySelector("#tiedot");
+  tiedot.innerHTML = `${username} ${password1}`;
 
   return false;
 };
-
-console.log();
-//btnSignIn.onclick = function () {
-// document.querySelector("lomake").reset();};
